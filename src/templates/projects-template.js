d7bb9d3fa78fb41d56361dfc52c41c8c5ca101c6
nikkig124 <nikkig124@gmail.com>
2020-10-27 13:43:45 -0500

    initial setup

---
+++ src/templates/projects-template.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { graphql, Link } from "gatsby"
+import Layout from "../components/Layout"
+import ReactMarkdown from "react-markdown"
+
+const ComponentName = () => {
+  return <h2>projects post template</h2>
+}
+
+// export const query = graphql`
+//   query GetSingleProject($slug: String) {
+//     blog: strapiProjects(slug: { eq: $slug }) {
+//       content
+
+//     }
+//   }
+// `
+ß
+export default ComponentName
