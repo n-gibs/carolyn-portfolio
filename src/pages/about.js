d6998314a7221ef3d1885d0daaa443d9deed4234
nikkig124 <nikkig124@gmail.com>
2020-11-03 11:37:41 -0600

    about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,46 @@ import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Title from "../components/Title"
 import Image from "gatsby-image"
-// ...GatsbyImageSharpFluid
-const About = () => {
-  return <h2>about page</h2>
+import SEO from "../components/SEO"
+
+const About = ({
+  data: {
+    about: { nodes },
+  },
+}) => {
+  const { title, description, image } = nodes[0]
+  return (
+    <Layout>
+    <SEO title="About" description="about Nicole"/>
+      <section className="about-page">
+        <div className="section-center about-center">
+          <Image fluid={image.childImageSharp.fluid} className="about-img" />
+          <article className="about-text">
+            <Title title={title} />
+            <p>{description}</p>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    about: allStrapiAbout {
+      nodes {
+        title
+        description
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default About
